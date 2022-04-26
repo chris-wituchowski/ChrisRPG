481436c677a8c61990b8f8263039745a00392841
Chris Wituchowski <chriswituchowski@outlook.com>
2022-04-25 22:47:01 -0700

    Cleanup and add reference to Engine proj

--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -1,8 +1,8 @@
 namespace SuperAdventure
 {
-    public partial class Form1 : Form
+    public partial class SuperAdventure : Form
     {
-        public Form1()
+        public SuperAdventure()
         {
             InitializeComponent();
         }
