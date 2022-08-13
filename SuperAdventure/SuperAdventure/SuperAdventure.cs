6b68fb58e789c583c9e3cb9489cac41e7fdba876
Chris Wituchowski <chriswituchowski@outlook.com>
2022-08-13 14:18:05 -0700

    Add Player class and form labels

--- SuperAdventure/SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure/SuperAdventure.cs
@@ -6,5 +6,10 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            lblGold.Text = "123";
+        }
     }
 }
